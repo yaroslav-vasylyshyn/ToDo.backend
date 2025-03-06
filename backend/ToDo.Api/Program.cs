1b34f2ac59b9f25a62047a98132b8d8fe13e0151
yaroslav-vasylyshyn <146115906+yaroslav-vasylyshyn@users.noreply.github.com>
2025-03-07 00:29:14 +0200

    Scaffolded the db, added connection string, added service to Program.ca

--- backend/ToDo.Api/Program.cs
+++ backend/ToDo.Api/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using ToDo.Core.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
