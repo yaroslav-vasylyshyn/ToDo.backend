3f9915557be42af375909c7408b891470571e6d1
yaroslav-vasylyshyn <146115906+yaroslav-vasylyshyn@users.noreply.github.com>
2025-03-11 12:03:44 +0200

    Added validation to CreateTaskDto and UpdateTaskDto
    added validation on task name and description fields, name must be in range of 2-255 characters, description must be in range of 2-500 characters.

--- backend/ToDo.Infrastructure/DTO/CreateTaskDto.cs
+++ backend/ToDo.Infrastructure/DTO/CreateTaskDto.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ToDo.Infrastructure.DTO;
 
 public partial class CreateTaskDto
 {
+    [MaxLength(255, ErrorMessage = "Task Name has to be a maximum of 255 characters long")]
+    [MinLength(2, ErrorMessage = "Task Name has to be a minimum of 2 characters long")]
     public string Name { get; set; } = null!;
 
+    [MaxLength(500, ErrorMessage = "Task Description has to be a maximum of 255 characters long")]
+    [MinLength(2, ErrorMessage = "Task Description has to be a minimum of 2 characters long")]
     public string Description { get; set; } = null!;
 
     public string Status { get; set; } = null!;
--- backend/ToDo.Infrastructure/DTO/TaskDto.cs
+++ backend/ToDo.Infrastructure/DTO/TaskDto.cs
@@ -1,11 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ToDo.Infrastructure.DTO;
 
 public partial class TaskDto
 {
     public int Id { get; set; }
-
     public string Name { get; set; } = null!;
-
     public string Description { get; set; } = null!;
 
     public string Status { get; set; } = null!;
